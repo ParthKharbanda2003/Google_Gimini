a8ff263f59750582e4e59e0baad0dc877af81fa3
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-06-23 04:07:08 +0530

    after adding functionalities

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 function Main() {
+
+
+
+  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} =useContext(Context)
+
+
   return (
     <div className='main'>
 
@@ -15,6 +22,8 @@ function Main() {
         
         <div className="main-container">
 
+            {!showResult
+            ?<>
             <div className="greet">
                 <p><span>Hello, Dev.</span></p>
                 <p>How can i help you today?</p>
@@ -51,16 +60,39 @@ function Main() {
                 </div>
 
             </div>
+            </>
+            :<div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+
+                    {loading
+                    ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                      </div>
+                      : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                       }
+
+                </div>
+             </div>    
+            }
+
+            
             
             <div className="main-bottom">
 
                 <div className="search-box">
 
-                    <input type="text" placeholder='Enter a prompt here'/>
+                    <input onChange={(e) => setInput(e.target.value)}  value={input} type="text" placeholder='Enter a prompt here'/>
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
                     </div>
 
                 </div>
---
+++ src/context/Context.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) =>{
+
+    const [input, setInput] =useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const[prevPrompts, setPrevPrompts] =useState([]);
+    const[showResult, setShowResult] =useState(false);
+    const[loading, setLoading] =useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const onSent = async (prompt) => {
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+
+        const response = await run(input)
+
+        setResultData(response)
+        setLoading(false)
+        setInput("")
+    }
+ 
+    const contextvalue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+    }
+
+    return (
+        <Context.Provider value={contextvalue}> 
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider
