21b8008a1ed59ea6d24b9a0e5fa91ce51d269ea4
ParthKharbanda2003 <PARTHKHARBANDA111.PK@GMAIL.COM>
2024-06-22 21:32:48 +0530

    only ui , no functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar.jsx'
+import Main from './components/Main/Main.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <p>hello this is google gemini</p>
+      <Sidebar/>
+      <Main/>
     </>
   )
 }
